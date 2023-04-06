1e247854026a5169eff3abc10089d66957246675
LEVOTHANHLAM <115397158+LEVOTHANHLAM@users.noreply.github.com>
2023-04-06 10:33:22 +0100

    06-04-2023

--- APP_QUANLY_KTX/APP_QUANLY_KTX/Forms/frmDSNhanVien.cs
+++ APP_QUANLY_KTX/APP_QUANLY_KTX/Forms/frmDSNhanVien.cs
@@ -1,18 +1,6 @@
-﻿using DevExpress.XtraEditors;
-using ProjectQLKTX.CallApis;
-using ProjectQLKTX.Interface;
+﻿using ProjectQLKTX.Interface;
 using ProjectQLKTX.Models;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static DevExpress.Xpo.Helpers.AssociatedCollectionCriteriaHelper;
 
 namespace ProjectQLKTX
 {
