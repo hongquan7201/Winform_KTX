f38a12dfb3004461f54f052b0a4acc41730a4afa
lam le <levothanhlam2001@gmail.com>
2023-04-18 11:20:53 +0700

    18-04-2023

--- APP_QUANLY_KTX/APP_QUANLY_KTX/Logins/LoginHelper.cs
+++ APP_QUANLY_KTX/APP_QUANLY_KTX/Logins/LoginHelper.cs
@@ -13,24 +13,16 @@ namespace ProjectQLKTX.Logins
     {
         public async Task<APIRespone<Nhanvien>> Login(Account account)
         {
-            try
-            {
-                HttpClient httpClient = new HttpClient();
-                httpClient.BaseAddress = new Uri(Constant.Domain);
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
-                var json = JsonConvert.SerializeObject(account, jsonSerializerSettings);
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                var response = await httpClient.PostAsync($"api/nhanvien/login", content);
-                var body = await response.Content.ReadAsStringAsync();
-                APIRespone<Nhanvien> data = JsonConvert.DeserializeObject<APIRespone<Nhanvien>>(body);
-                return data;
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, ex.Message);
-                return null;
-            }
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(Constant.Domain);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+            var json = JsonConvert.SerializeObject(account, jsonSerializerSettings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync($"api/nhanvien/login", content);
+            var body = await response.Content.ReadAsStringAsync();
+            APIRespone<Nhanvien> data = JsonConvert.DeserializeObject<APIRespone<Nhanvien>>(body);
+            return data;
 
         }
     }
